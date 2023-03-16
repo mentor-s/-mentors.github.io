7cc4938a2b1afd0bf7882fbb4532d470e84f5cad
WoojinDeve <jwj060111@gmail.com>
2023-03-16 19:48:42 +0900

    refactor: 설정 변경

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "mentors.log",
   description: "멘토스팀 기술 블로그입니다.",
   author: "mentors",
-  siteUrl: "https://mentors.github.io/",
+  siteUrl: "https://the-mentors.github.io/",
   links: {
     github: "https://github.com/the-mentors",
   },
