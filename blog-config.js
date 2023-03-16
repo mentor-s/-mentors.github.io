3d814e0363ab86a17b790b9e9115867179e0e479
WoojinDeve <jwj060111@gmail.com>
2023-03-16 19:32:25 +0900

    init: blog intialize

---
+++ blog-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "mentors.log",
+  description: "멘토스팀 기술 블로그입니다.",
+  author: "mentors",
+  siteUrl: "https://mentors.github.io/",
+  links: {
+    github: "https://github.com/the-mentors",
+  },
+  utterances: {
+    repo: "the-mentors/the-mentors.github.io",
+    type: "pathname",
+  },
+};
